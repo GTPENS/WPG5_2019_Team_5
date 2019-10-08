f8496d2f34aa71533b233efbf34402266ae591e6
Rahmat Ansori <ansori34@gmail.com>
2019-10-08 15:46:02 +0700

    Add Network Manager

---
+++ Assets/Scripts/Manager/NetworkManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Net.Sockets;
+using System;
+
+public class NetworkManager : MonoBehaviour
+{
+    bool socketReady = false;
+
+    TcpClient mySocket;
+    NetworkStream theStream;
+    StreamWriter theWriter;
+    StreamReader theReader;
+    string HOST = "localhost";
+    int PORT = 8080;
+
+    public void Start()
+    {
+        setupSocket();
+        writeSocket("test");
+        Debug.Log($"Response: {readSocket()}");
+    }
+
+    public bool setupSocket() 
+    {
+        try {
+            mySocket = new TcpClient(HOST, PORT);
+            theStream = mySocket.GetStream();
+            theWriter = new StreamWriter(theStream);
+            theReader = new StreamReader(theStream);
+            socketReady = true;
+
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void writeSocket(string theLine) 
+    {
+        if (!socketReady)
+            return;
+
+        string tmpString = theLine + "\r\n";
+        theWriter.Write(tmpString);
+        theWriter.Flush();
+    }
+
+    public string readSocket() 
+    {
+        if (!socketReady)
+            return "";
+        
+        if (theStream.DataAvailable)
+            return theReader.ReadLine();
+        
+        return "";
+    }
+
+    public void closeSocket() 
+    {
+        if (!socketReady)
+            return;
+        
+        theWriter.Close();
+        theReader.Close();
+        mySocket.Close();
+        socketReady = false;
+    }
+ 
+    public void maintainConnection() 
+    {
+        if(!theStream.CanRead) {
+            setupSocket();
+        }
+    }
+}
